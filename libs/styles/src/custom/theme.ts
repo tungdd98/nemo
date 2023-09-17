3b732829ba20a8e1004c1d4480e38f3798479297
tungdd <tungdd98@gmail.com>
2023-09-17 20:06:48 +0700

    Override css

--- libs/styles/src/custom/theme.ts
+++ libs/styles/src/custom/theme.ts
@@ -5,4 +5,24 @@ import { typographyOptions } from './typography';
 export const themeOptions: ThemeOptions = {
   palette: paletteOptions,
   typography: typographyOptions,
+  components: {
+    MuiInputBase: {
+      styleOverrides: {
+        root: ({ ownerState }) => ({
+          ...(ownerState.size === 'small' && {
+            fontSize: '14px',
+          }),
+        }),
+      },
+    },
+    MuiInputLabel: {
+      styleOverrides: {
+        root: ({ ownerState }) => ({
+          ...(ownerState.size === 'small' && {
+            fontSize: '14px',
+          }),
+        }),
+      },
+    },
+  },
 };
